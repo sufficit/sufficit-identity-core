206b08cc539bd841e2e81cb167092f97b6d68673
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-04-01 10:49:09 -0300

    include service monitor directive

---
+++ src/Vendas/ServiceMonitorDirective.cs
@@ -0,0 +1,26 @@
+﻿using Sufficit.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Vendas
+{
+    /// <summary>
+    /// Used to include on group Sales Representative 
+    /// </summary>
+    public class ServiceMonitorDirective : Directive
+    {
+        public const string UniqueID = "362a9b8f-310c-4531-8103-7cca22a1d2f5";
+        public const string RoleID = SalesRepresentativeRole.UniqueID;
+
+        public override Guid ID => Guid.Parse(UniqueID);
+
+        public override Guid IDRole => Guid.Parse(RoleID);
+
+        public override string Name => "monitorar serviços de cliente";
+
+        public override string Description => "Usado para incluir no Grupo de Representantes de vendas.";
+
+        public override string Key => "servicemonitor";
+    }
+}
