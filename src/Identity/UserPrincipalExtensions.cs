f52fe8eed3ee718955e6901dfaf0043f2a8902f3
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-04-05 20:12:45 -0300

    user principal extensions

---
+++ src/Identity/UserPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Identity
+{
+    public static class UserPrincipalExtensions
+    {
+        public static bool IsInRole(this UserPrincipal source, IRole role) => source.Roles.Contains(role);
+
+        public static bool IsInRole<T>(this UserPrincipal source) where T : IRole, new()
+        {
+            return source.Roles.Contains(new T());
+        }
+
+        public static bool IsInRole(this UserPrincipal source, IEnumerable<IRole> roles)
+        {
+            foreach (var role in roles)
+                if (source.IsInRole(role)) return true;
+            return false;
+        }
+    }
+}
