c1a42ad77a82dd26b2f4f01ebdb14a1bc5ef81a6
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-11-16 19:48:26 -0300

    balance view directive

---
+++ src/Finance/BalanceViewDirective.cs
@@ -0,0 +1,21 @@
+﻿using Sufficit.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Finance
+{
+    public class BalanceViewDirective : Directive
+    {
+        public const string UniqueID = "1c8a1f49-6f3e-4979-8885-d52fc79f0dee";
+        public const string RoleID = FinancialRole.UniqueID;
+
+        public override Guid ID => Guid.Parse(UniqueID);
+
+        public override Guid IDRole => Guid.Parse(RoleID);
+
+        public override string Name => "visualizar saldo";
+
+        public override string Key => "balanceview";
+    }
+}
