aa843d5899dab6c01408048ebc220eb11f859a15
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-01-18 21:54:34 -0300

    packages updates;

--- src/Identity/Role.cs
+++ src/Identity/Role.cs
@@ -7,19 +7,24 @@ namespace Sufficit.Identity
 {
     public static class Role
     {        
+        public static IEnumerable<IRole> Enumerator { get; }
+            = Utils.GetCollectionOfType<IRole>();
+
         public static bool Compare(this IRole source, string role)
         {
             if (!string.IsNullOrWhiteSpace(role))
             {
                 string newRole = role.ToLowerInvariant().Trim();
-                if (source.Filter.Contains(newRole)){
+                if (Guid.TryParse(role, out Guid roleId))
+                {
+                    if (source.ID == roleId)
+                        return true;
+                }
+                else if (source.Filter.Contains(newRole)){
                     return true;
                 }
             }
             return false;
         }
-
-        public static IEnumerable<IRole> Enumerator { get; }
-            = Utils.GetCollectionOfType<IRole>();
     }
 }
