a1c324c703ab11d14e2acb87c250b471d4307b72
hugodeco <hugodeco@sufficit.com.br>
2023-04-22 21:53:03 -0300

    provisioning

---
+++ src/Provisioning/ProvisioningAdminDirective.cs
@@ -0,0 +1,19 @@
+﻿using Sufficit.Identity;
+using System;
+
+namespace Sufficit.Provisioning
+{
+    public class ProvisioningAdminDirective : Directive
+    {
+        public const string UniqueID = "2353d733-50ff-4114-9626-d0120bcc5063";
+        public const string RoleID = ProvisioningRole.UniqueID;
+
+        public override Guid ID => Guid.Parse(UniqueID);
+
+        public override Guid IDRole => Guid.Parse(RoleID); 
+
+        public override string Name => "administrar provisionamento";
+
+        public override string Key => "provisioningadmin";
+    }
+}
---
+++ src/Provisioning/ProvisioningRole.cs
@@ -0,0 +1,20 @@
+﻿using Sufficit.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Provisioning
+{
+    public struct ProvisioningRole : IRole
+    {
+        public const string UniqueID = "2c5db110-a92e-4757-8655-919d50aa043e";
+
+        public Guid ID => Guid.Parse(UniqueID);
+
+        public string Name => "Provisioning";
+
+        public string NormalizedName => "provisioning";
+
+        string[] IRole.Filter => new string[] { NormalizedName };
+    }
+}
