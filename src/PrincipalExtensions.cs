552e9500a5684733d79a18ad9bf1a0519d739347
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2024-01-22 16:26:56 -0300

    packages adjusts;

---
+++ src/PrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Sufficit.Identity
+{
+    public static class PrincipalExtensions
+    {
+        public static bool IsManager(this ClaimsPrincipal principal)
+        {
+            return
+                principal.IsInRole(ManagerRole.NormalizedName) ||
+                principal.IsInRole(AdministratorRole.NormalizedName);
+        }
+    }
+}
