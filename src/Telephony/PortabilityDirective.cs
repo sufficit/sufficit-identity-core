0fc3a8b02f852508f546a15d48f7114344ec3b99
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-04-25 14:34:36 -0300

    Adiciona suporte a portabilidade e melhorias gerais
    Este commit inclui a adição de novas constantes na classe `Constants.cs` para dados de telefonia, além de melhorias na serialização e novos campos na classe `EndPointPagedResponse`. O arquivo `ILimit.cs` foi atualizado para melhor documentação.
    
    A classe `CallBack` agora implementa interfaces adicionais, e `DIDSearchParameters` foi expandido com novos campos para busca. A classe `Destination` foi aprimorada para lógica de comparação e hash.
    
    Foi introduzido o `PortabilityController` e as classes `PortabilityProcess` e `PortabilityProcessStatus` para gerenciar processos de portabilidade. O `ServiceCollectionExtensions.cs` foi atualizado para incluir o `PortabilityProvider` e o contexto do banco de dados `EFTelephonyAdminDBContext`.
    
    Além disso, um conversor para `TextFilter` foi adicionado ao `JsonSerializer`, e o arquivo `appsettings.json` foi modificado para incluir um novo número na lista de `BlackList`. Melhorias de documentação e ajustes de estilo foram realizados em todo o código.

---
+++ src/Telephony/PortabilityDirective.cs
@@ -0,0 +1,24 @@
+﻿using Sufficit.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Telephony
+{
+    /// <summary>
+    ///     Portability Management
+    /// </summary>
+    public class PortabilityDirective : Directive
+    {
+        public const string UniqueID = "fe921659-0606-4c43-982f-0c3baa5cf90a";
+        public const string RoleID = TelephonyManagerRole.UniqueID;
+
+        public override Guid ID => Guid.Parse(UniqueID);
+
+        public override Guid IDRole => Guid.Parse(RoleID);
+
+        public override string Name => "gerenciar processos de portabilidade";
+
+        public override string Key => "portability";
+    }
+}
