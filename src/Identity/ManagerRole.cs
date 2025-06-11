edae19f49a2dcf66463fd37c28522d09c4dc1363
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-11 10:38:17 -0300

    Adiciona comentários XML à estrutura ManagerRole
    Foram incluídos comentários que descrevem a estrutura `ManagerRole`, detalhando seu papel como gerente do sistema, identificador único, nome exibido, nome normalizado e filtros associados. A estrutura também implementa a interface `IRole` para garantir compatibilidade com sistemas baseados em papéis.

--- src/Identity/ManagerRole.cs
+++ src/Identity/ManagerRole.cs
@@ -4,6 +4,12 @@ using System.Text;
 
 namespace Sufficit.Identity
 {
+    /// <summary>
+    /// Represents the role of a system manager within the application.
+    /// </summary>
+    /// <remarks>This struct provides information about the system manager role, including its unique
+    /// identifier, display name, normalized name, and associated filters. It implements the <see cref="IRole"/>
+    /// interface to ensure compatibility with role-based systems.</remarks>
     public struct ManagerRole : IRole
     {
         public const string UniqueID = "9cdc6bbe-6be0-4a76-a15c-2638b4125175";
