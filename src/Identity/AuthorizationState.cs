e1c255c9b2f2baa0294b5d45adcee3274b4127d9
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-06-28 19:06:15 -0300

    Adiciona suporte a PersistedGrant e refatora código
    Implementa novas classes para gerenciamento de dados de autorização, incluindo PersistedGrant, ReferenceToken e AuthorizationState. Atualiza EFIdentityDBContext para incluir DbSet<PersistedGrant> e configurações no ModelBuilderHelper. Adiciona métodos na classe IdentityEFProvider para manipulação de grants persistidos e melhora a legibilidade do código com atualizações de logging e refatoração de métodos. Ajustes de estilo e formatação também foram realizados para maior consistência.

---
+++ src/Identity/AuthorizationState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Sufficit.Identity
+{
+    /// <summary>
+    /// Represents the state of an authorization process, including a unique identifier and an optional return URL.
+    /// </summary>
+    public class AuthorizationState
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("returnUrl")]
+        public string? ReturnUrl { get; set; }
+    }
+}
---
+++ src/Identity/TokenResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Sufficit.Identity
+{
+    /// <summary>
+    /// Represents the response received from an authentication server when requesting an access token.
+    /// </summary>
+    /// <remarks>This class encapsulates the details of a token response, including the access token, its
+    /// expiration time, the token type, the associated scope, and an optional refresh token. It is typically used in
+    /// OAuth2 or similar authentication flows.</remarks>
+    public class TokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = default!;
+
+        [JsonPropertyName("id_token")]
+        public string? IdToken { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = default!;
+
+        [JsonPropertyName("scope")]
+        public string Scope { get; set; } = default!;
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; } = default!;
+    }
+}
