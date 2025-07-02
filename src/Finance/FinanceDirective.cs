f9ab3b3af0a67a6171d7b79e591e557c43cbedd7
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2025-07-02 18:47:40 -0300

    Refatorar diretiva de faturamento para finanças
    Alterado o namespace de `Sufficit.Billing` para `Sufficit.Finance` para refletir o novo foco em funcionalidades financeiras. A classe `BillingDirective` foi renomeada para `FinanceDirective`, e sua descrição foi atualizada para indicar acesso a operações financeiras. As propriedades `Name` e `Key` também foram modificadas para alinhar com a nova terminologia.

---
+++ src/Finance/FinanceDirective.cs
@@ -0,0 +1,23 @@
+﻿using Sufficit.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sufficit.Finance
+{
+    /// <summary>
+    /// Represents a directive that provides access to finance-related functionality.
+    /// </summary>
+    /// <remarks>This directive is identified by a unique ID and key, and is used to enable or manage finances
+    /// operations.</remarks>
+    public class FinanceDirective : Directive
+    {
+        public const string UniqueID = "20b48f3b-d839-4007-bde4-38151740c6a9";
+
+        public override Guid ID => Guid.Parse(UniqueID);
+
+        public override string Name => "acesso ao financeiro";
+
+        public override string Key => "finance";
+    }
+}
