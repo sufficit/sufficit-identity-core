e91b9656c9ec9f09cadb388d2f8495c3871ebf74
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2026-08-02 21:01:17 -0300

    feat: add GatewayDiagnosticsDirective for provider diagnostics

---
+++ src/Gateway/GatewayDiagnosticsDirective.cs
@@ -0,0 +1,19 @@
+using Sufficit.Identity;
+using System;
+
+namespace Sufficit.Gateway
+{
+    /// <summary>
+    /// Allows an administrator to execute audited, allow-listed provider diagnostics.
+    /// </summary>
+    public sealed class GatewayDiagnosticsDirective : Directive
+    {
+        public const string UniqueID = "1dc24b3a-c724-40c1-9793-cc8a74ad61c9";
+        public const string RoleID = AdministratorRole.UniqueID;
+
+        public override Guid ID => Guid.Parse(UniqueID);
+        public override Guid IDRole => Guid.Parse(RoleID);
+        public override string Name => "depurar gateways";
+        public override string Key => "gatewaydiagnostics";
+    }
+}
